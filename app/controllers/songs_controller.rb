5f3fd7baec8f8b1d7f4ae20e8fe3ef39385d654b
Yomaira Escano <yjescano@gmail.com>
2018-03-12 14:32:41 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,10 +3,10 @@ class SongsController < ApplicationController
   def index
     @songs = Song.all
   end
-  # 
-  # def show
-  #   @song = Song.find(params[:id])
-  # end
+  
+  def show
+    @song = Song.find(params[:id])
+  end
   # 
   # def new
   #   @song = Song.new
