6ae396a1587e90f677892493c1323cf95e491ad1
Yomaira Escano <yjescano@gmail.com>
2018-03-12 14:23:03 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,8 @@
 class Song < ActiveRecord::Base
   # validates :title, presence: true
-  with_options
+  with_options if: :is_released? do |song|
+    song.validates :release_year, presence: true, numericality: {less_than_or_equal_to: DateTime.now.year}
+  end
 
   def is_released?
     released
