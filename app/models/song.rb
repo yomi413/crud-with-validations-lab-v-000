a30f13c585abb781512b6ce3cc1b0398f7ea67ca
Yomaira Escano <yjescano@gmail.com>
2018-03-12 14:10:54 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,16 +1,2 @@
 class Song < ActiveRecord::Base
-  validates :title, presence: true, uniqueness: true
-  validates :artist_name, presence: true
-  # validate :is_released?
-  # binding.pry
-
-  # def is_released?
-  #
-  #   if released == false
-  #     "optional"
-  #   elsif released == true
-  #     release_year <=> DateTime.now.year
-  #   end
-  # end
-
 end
